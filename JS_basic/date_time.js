4c56ae4bd0cce59b2ed1a4a0312729ace0a20d1b
Mohit Bora <boramohit3@gmail.com>
2024-01-17 22:32:09 +0530

    This is date and time basics

---
+++ JS_basic/date_time.js
@@ -0,0 +1,27 @@
+
+let date = new Date()
+// console.log(date.toString())
+// console.log(date.toISOString())
+// console.log(date.toDateString())
+// console.log(date.toLocaleString())
+
+// let myDate = new Date(2024,2,15)
+// console.log(myDate.toLocaleString())
+
+let timeStamp = Date.now()
+// console.log(timeStamp)
+
+// Note: timeStamp gives the time in millisecond from 1 jan 1970
+
+// console.log(date.getTime())
+// let dd = new Date(timeStamp)
+// console.log(dd.toLocaleDateString())
+
+// Note: we can convert a time stamp to date using Date constructor
+
+let today = new Date();
+console.log(today.getDate())
+console.log(today.getMonth())
+console.log(today.getFullYear())
+
+
